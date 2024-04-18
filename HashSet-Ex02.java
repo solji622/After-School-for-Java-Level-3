3043b0db0570bf0c4cb946af84868ea12c751763
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-04-18 18:37:55 +0900

    Create HashSet-Ex02.java

---
+++ HashSet-Ex02.java
@@ -0,0 +1,38 @@
+package hash;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSetEx02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		int N = sc.nextInt();
+		int [] numbers = new int[N];
+		for (int i = 0; i <N; i++) {
+			numbers[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < numbers.length-1; i++) {
+			for (int j = i+1; j < numbers.length; j++) {
+				set.add(numbers[i] + numbers[j]);
+			}
+		}
+		
+		System.out.println(set);
+
+    Iterator iter = set.iterator();
+		while (iter.hasNext()) {
+			System.out.print(iter.next() + " ");
+		}
+		System.out.println();
+
+    int[] newNums = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+		
+		for (int n : newNums) {
+			System.out.print(n + " ");
+		}
+	}
+}
