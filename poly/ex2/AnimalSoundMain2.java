5881e2c490a8b6a47e10487a1662ea0be59cdeff
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-05-16 18:31:43 +0900

    Add files via upload

---
+++ poly/ex2/AnimalSoundMain2.java
@@ -0,0 +1,18 @@
+package poly.ex2;
+
+public class AnimalSoundMain2 {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		Cow cow = new Cow();
+		
+		Animal [] animals = {dog, cat, cow};
+		
+		for (Animal a : animals) {
+			System.out.println("동물 소리 테스트 시작");
+			a.sound();
+			System.out.println("동물 소리 테스트 종료");
+			System.out.println();
+		}
+	}
+}
