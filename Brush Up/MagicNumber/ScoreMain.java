7a5967d71398c12ec50e44ec1aad7023351836b4
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-06-06 17:47:19 +0900

    Create ScoreMain.java

---
+++ Brush Up/MagicNumber/ScoreMain.java
@@ -0,0 +1,10 @@
+package MagicNumber;
+
+import java.util.Scanner;
+
+public class ScoreMain {
+    public static void main(String[] args) {
+        Score score = new Score();
+        score.calculateAverage();
+    }
+}
