f5f04de2a6916b278f8cc7e372671fcefb3e4143
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-06-11 13:58:51 +0900

    Create Course.java

---
+++ Brush Up/exam/learnOnline/Course.java
@@ -0,0 +1,38 @@
+package learnOnline;
+
+public class Course implements Learnable{
+	private String title;
+	private int time;
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public Course(String title, int time) {
+		this.title = title;
+		this.time = time;
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Course start: " + title);
+		
+	}
+
+	@Override
+	public void pause() {
+		System.out.println("Course pause: " + title);
+		
+	}
+
+	@Override
+	public void complete() {
+		System.out.println("Course complete: " + title);
+		
+	}
+	
+}
